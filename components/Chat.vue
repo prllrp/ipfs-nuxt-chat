f06ea310159dbc99c52b04eac4200275584136ed
patricklane09@gmail.com <patricklane09@gmail.com>
2022-08-04 14:48:36 -0500

    Chat layout

---
+++ components/Chat.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="chat">
+        <div class="chat-header">
+            <div class="chat-header-title">
+                <h3>{{ title }}</h3>
+            </div>
+            <div class="chat-header-buttons">
+                <button class="btn btn-primary" @click="sendMessage">Send</button>
+            </div>
+        </div>
+        <div class="chat-body">
+            <div class="chat-body-messages">
+                <div v-for="message in messages" class="chat-body-message">
+                    <div class="chat-body-message-text">
+                        {{ message.text }}
+                    </div>
+                    <div class="chat-body-message-time">
+                        {{ message.time }}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="chat-footer">
+            <div class="chat-footer-input">
+                <input type="text" v-model="message" @keyup.enter="sendMessage" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+const messages = [];
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
